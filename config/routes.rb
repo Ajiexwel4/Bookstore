6805db654e41932f313cf3c965aa7e7ffdddd405
Ajiexwel4 <ajiexwel4@gmail.com>
2017-02-21 18:17:23 +0200

    some fixes in routes.rb and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'catalog/index'
-
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   ActiveAdmin.routes(self)
 
+  get 'catalog/index'
   root 'home#index'
 
-  get 'store/index'
 
   resources :books
   resources :orders
