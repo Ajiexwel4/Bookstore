e600e1c65bccdab97e129b87c63a88fa785f12aa
Ajiexwel4 <ajiexwel4@gmail.com>
2017-02-24 12:34:04 +0200

    some changes in order

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
   belongs_to :book
   belongs_to :cart
+  belongs_to :order
 
   default_scope { order(:id) }
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,19 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+  belongs_to :user
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
+
+  def order_number(order)
+    order.number = 'R' + order.id
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end
