07516930bbe6674e6bb2e111b335e848b75386b2
Ajiexwel4 <ajiexwel4@gmail.com>
2017-02-24 13:24:18 +0200

    carousel latest 3 books

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < InheritedResources::Base
 
+  def create
+    @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
+
+    respond_to do |format|
+      if @order.save
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to root_path, notice: 'Thank you for your order.' }
+        format.json { render action: 'show', status: :created, location: @order }
+      else
+        @cart = current_cart
+        format.html { render action: 'new' }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+  end
+
   private
 
     def order_params
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def button_class_if(book)
+    book.in_stock.zero? ? 'btn btn disabled' : 'btn btn-primary'
+  end
 end
